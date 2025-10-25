58915fffa2d2268d024bab8296cc43e0631033e0
ConTresillo <tharoonsays@gmail.com>
2025-10-26 02:08:12 +0530

    Remove unwanted files

--- BackEnd/api.py
+++
@@ -1,9 +0,0 @@
-# api.py.py
-
-#Header
-
-def main():
-    pass
-
-if __name__ == '__main__':
-    main()
--- BackEnd/test_database.py
+++
@@ -1,9 +0,0 @@
-# test_database.py.py
-
-#Header
-
-def main():
-    pass
-
-if __name__ == '__main__':
-    main()
